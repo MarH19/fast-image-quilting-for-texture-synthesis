52ac6ea10dbbdd73e87734229233b05902bf90b9
Lukas Nüesch <lnueesch@student.ethz.ch>
2023-03-31 12:17:02 +0200

    imread

---
+++ imread.c
@@ -0,0 +1,27 @@
+#define STB_IMAGE_IMPLEMENTATION
+#include <stdio.h>
+#include "imread.h"
+#include "stb_image.h"
+
+image_int8 imread(char* path)
+{
+    int width, height, channels;
+    unsigned char *image = stbi_load(path, &width, &height, &channels, 0);
+
+    image_int8 in_image = {image, width, height, channels};
+
+    stbi_image_free(image);
+
+    return in_image;
+}
+
+int main() {
+    image_int8 in_image = imread("image.jpg");
+    printf("Image width: %d\n", in_image.width);
+    printf("Image height: %d\n", in_image.height);
+    printf("Number of channels: %d\n", in_image.channels);
+
+    return 0;
+}
+
+// gcc test.c -o test -lm
---
+++ imread.h
@@ -0,0 +1,6 @@
+typedef struct{
+    unsigned char* data;
+    int width;
+    int height;
+    int channels;
+} image_int8;
