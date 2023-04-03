dfb45920adbb5513d6a71ee9f4f2171a8662afb9
Lukas Nüesch <lnueesch@student.ethz.ch>
2023-04-03 17:12:10 +0200

    dpcut horizontal case working

--- dpcut.c
+++ dpcut.c
@@ -6,43 +6,148 @@
 // slice_1 and slice_2 of same size are merged, c = 0 for vertical case, c = 1 for horizontal case
 void calcErrors(slice_t slice_1, slice_t slice_2, pixel_t *errors);
 
-pixel_t* dpcut(slice_t slice_1, slice_t slice_2, int c)
+// this implementation currently only works for vertical overlapping blocks
+void dpcut(slice_t slice_1, slice_t slice_2, slice_t out, int c)
 {
     // initialize errors array
-    pixel_t *errors = malloc(sizeof(pixel_t) * slice_1.width * slice_1.height);
+    // does /3 work
+    int width = slice_1.width;
+    int height = slice_1.height;
+    pixel_t *errors = malloc(sizeof(pixel_t) * width * height);
+
     calcErrors(slice_1, slice_2, errors);
-    
+
     if (c == 1)
     {
         // transpose errors
+
+        // probably best/easier to just code two cases
     }
 
     // initialize dp array
+    pixel_t *dp = malloc(sizeof(pixel_t) * width * height);
+
+    // initialize the first row of the dp array with the first row of errors
+    for (int i = 0; i < width; i++)
+    {
+        dp[i] = errors[i];
+    }
+
+    // watch out this changes when transpose
+    pixel_t max_val = pow(255, 2) * 3 * height + 1;
 
     // fill dp table
-
-    // do backtracking while filling slice_1 -- aliasing a problem?
-
-    if (c == 1)
+    for (int i = 1; i < height; i++)
     {
-        // traspose back?? how can i do this while i write?
+        for (int j = 0; j < width; j++)
+        {
+            // create array of size 3 for the 3 possible paths
+            // since we take the min i initialize with the maximum possible value
+            pixel_t paths[3] = {max_val, max_val, max_val};
+            paths[1] = dp[(i - 1) * width + j];
+
+            if (j != 0)
+            {
+                paths[0] = dp[(i - 1) * width + j - 1];
+            }
+
+            if (j != width - 1)
+            {
+                paths[2] = dp[(i - 1) * width + j + 1];
+            }
+
+            // calculate min element of paths
+            // precendence: middle, left, right
+            pixel_t min_path = paths[0] < paths[1] ? (paths[0] < paths[2] ? paths[0] : paths[2]) : (paths[2] < paths[1] ? paths[2] : paths[1]);
+            dp[i * width + j] = errors[i * width + j] + min_path;
+        }
     }
-    return errors;
+    //return errors;
+    //return dp;
+    // do backtracking while filling out
+    // aliasing shouldn't be a problem but i need to write update the out slice/image
+
+    // find the min element in the last row of the dp table
+    int start;
+    pixel_t min = max_val;
+
+    for (int i = 0; i < width; i++)
+    {
+        pixel_t temp_min = dp[(height - 1) * width + i] < max_val ? dp[(height - 1) * width + i] : max_val;
+        if (temp_min < min)
+        {
+            start = i;
+            min = temp_min;
+        }
+    }
+    // set last row of out image
+    for (int i = 0; i < slice_1.width * 3; i++)
+    {
+        out.data[(out.height - 1) * out.jumpsize + i] = i/3 < start ? slice_1.data[(height - 1) * slice_1.jumpsize + i] : slice_2.data[(slice_2.height - 1) * slice_2.jumpsize + i];
+    }
+
+
+    // for all rows above do
+    // find new start
+    // set current row of out image
+    for (int i = height - 2; i >= 0; i--)
+    {
+        // is the j loop needed?
+        // find minimum in 3 possible paths
+        pixel_t paths[3] = {max_val, max_val, max_val};
+        paths[1] = dp[i * width + start];
+
+        if (start != 0)
+        {
+            paths[0] = dp[i * width + start - 1];
+        }
+
+        if (start != width - 1)
+        {
+            paths[2] = dp[i * width + start + 1];
+        }
+
+        pixel_t min_path = paths[0] < paths[1] ? (paths[0] < paths[2] ? paths[0] : paths[2]) : (paths[2] < paths[1] ? paths[2] : paths[1]);
+
+        if (start < width - 1)
+        {
+            if (dp[width * i + start + 1] == min_path)
+            {
+                start++;
+            }
+        }
+        if (start > 0)
+        {
+            if (dp[width * i + start + -1] == min_path)
+            {
+                start--;
+            }
+        }
+        printf("%d\n", start);
+        // fill the row of the output
+        for (int j = 0; j < out.width * 3; j++)
+        {
+            out.data[i * out.jumpsize + j] = j/3 < start ? slice_1.data[i * slice_1.jumpsize + j] : slice_2.data[i * slice_2.jumpsize + j];
+        }
+    }
+    // free errors
+    free(errors);
+    // free dp
+    free(dp);
+
 }
 
 // errors(i,j) = sum of squared differences of the 3 rgb values
 void calcErrors(slice_t slice_1, slice_t slice_2, pixel_t *errors)
 {
-    
+
     for (int i = 0; i < slice_1.height; i++)
     {
-        for (int j = 0; j < slice_1.width; j++)
+        for (int j = 0; j < slice_1.width * 3; j += 3)
         {
             // ssd for one pixel
-            pixel_t error = (pow(slice_1.data[i*slice_1.jumpsize + j] - slice_2.data[i*slice_2.jumpsize + j], 2)) 
-                            + (pow(slice_1.data[i*slice_1.jumpsize + j + 1] - slice_2.data[i*slice_2.jumpsize + j + 1], 2)) 
-                            + (pow(slice_1.data[i*slice_1.jumpsize + j + 2] - slice_2.data[i*slice_2.jumpsize + j + 2], 2));
-            errors[i*slice_1.width + j] = error;
+            pixel_t error = (pow(slice_1.data[i * slice_1.jumpsize + j] - slice_2.data[i * slice_2.jumpsize + j], 2)) + (pow(slice_1.data[i * slice_1.jumpsize + j + 1] - slice_2.data[i * slice_2.jumpsize + j + 1], 2)) + (pow(slice_1.data[i * slice_1.jumpsize + j + 2] - slice_2.data[i * slice_2.jumpsize + j + 2], 2));
+            errors[i * slice_1.width + j/3] = error;
         }
     }
 }
--- imageio.c
+++ imageio.c
@@ -6,7 +6,7 @@
 #include "stb_image_write.h"
 
 extern double l2norm(slice_t inp_slice, slice_t out_slice);
-extern pixel_t *dpcut(slice_t slice_1, slice_t slice_2, int c);
+extern void dpcut(slice_t slice_1, slice_t slice_2, slice_t out, int c);
 
 image_t imread(char *path)
 {
@@ -84,72 +84,157 @@ slice_t slice_slice(slice_t sin, int start_row, int start_col, int end_row, int 
 int main()
 {
     image_t in_image = imread("image.jpg");
-    
+
     printf("Image width: %d\n", in_image.width);
     printf("Image height: %d\n", in_image.height);
     printf("Number of channels: %d\n", in_image.channels);
-    slice_t slice = slice_image(in_image, 50, 50, 70, 70);
-    printf("%d %d %d\n", slice.width, slice.height, slice.channels);
-    int n = slice.width * slice.height * slice.channels;
+    slice_t imslice1 = slice_image(in_image, 65, 50, 85, 70);
+    slice_t imslice2 = slice_image(in_image, 15, 10, 35, 30);
+    printf("%d %d %d\n", imslice1.width, imslice1.height, imslice1.channels);
+    int n = imslice1.width * imslice1.height * imslice1.channels;
     printf("%d\n", n);
+
+    // create the out array
+    pixel_t out[1200] = {0.0};
+    slice_t imslice_out = {out, 20, 20, 3, 60};
+    dpcut(imslice1, imslice2, imslice_out, 0);
+
     unsigned char *out_image = malloc(sizeof(unsigned char) * n);
     assert(out_image);
-    for (int row = 0; row < slice.height; row++)
-    {
-        for (int col = 0; col < slice.width * slice.channels; col++)
-        {
-            out_image[row * (slice.width * slice.channels) + col] = (unsigned char)slice.data[row * slice.jumpsize + col];
-        }
-    }
-    stbi_write_png("out1.jpg", slice.width, slice.height, slice.channels, out_image, slice.channels * slice.width);
 
-    // test l2norm
-    printf("%f\n", l2norm(slice, slice));
-    pixel_t arr1[] = {1.0, 1.0, 1.0, 1.0};
-    pixel_t arr2[] = {0.0, 0.0, 0.0, 0.0};
-    slice_t s1 = {arr1, 2, 2, 1, 2};
-    slice_t s2 = {arr2, 2, 2, 1, 2};
-    printf("%f\n\n", l2norm(s1, s2));
+    // write out
+    for (int row = 0; row < imslice1.height; row++)
+    {
+        for (int col = 0; col < imslice1.width * imslice1.channels; col++)
+        {
+            out_image[row * (imslice1.width * imslice1.channels) + col] = (unsigned char)imslice_out.data[row * imslice_out.jumpsize + col];
+        }
+    }
+    stbi_write_png("out1.jpg", imslice1.width, imslice1.height, imslice1.channels, out_image, imslice1.channels * imslice1.width);
 
-    // test errors
-    pixel_t matrix1[27] = {0.0};
-    pixel_t matrix2[27] = {1.0};
-    for (int i = 0; i < 27; i++)
+    // write in1
+    out_image = malloc(sizeof(unsigned char) * n);
+    assert(out_image);
+    for (int row = 0; row < imslice1.height; row++)
     {
-        matrix2[i] = 2.0;
-    }
-    slice_t slice1 = {matrix1, 3, 3, 3, 9};
-    slice_t slice2 = {matrix2, 3, 3, 3, 9};
-    for (int i = 0; i < slice1.height; i++)
-    {
-        for (int j = 0; j < slice1.width; j++)
+        for (int col = 0; col < imslice1.width * imslice1.channels; col++)
         {
-            printf("%f ", slice1.data[i * slice1.jumpsize + j]);
+            out_image[row * (imslice1.width * imslice1.channels) + col] = (unsigned char)imslice1.data[row * imslice1.jumpsize + col];
         }
-        printf("\n");
     }
-    printf("\n");
-    for (int i = 0; i < slice1.height; i++)
+    stbi_write_png("in1.jpg", imslice1.width, imslice1.height, imslice1.channels, out_image, imslice1.channels * imslice1.width);
+
+    // write in2
+    out_image = malloc(sizeof(unsigned char) * n);
+    assert(out_image);
+    for (int row = 0; row < imslice2.height; row++)
     {
-        for (int j = 0; j < slice1.width; j++)
+        for (int col = 0; col < imslice2.width * imslice2.channels; col++)
         {
-            printf("%f ", slice2.data[i * slice1.jumpsize + j]);
+            out_image[row * (imslice2.width * imslice2.channels) + col] = (unsigned char)imslice2.data[row * imslice2.jumpsize + col];
         }
-        printf("\n");
     }
-    printf("\n");
-    pixel_t *errors = dpcut(slice1, slice2, 0);
-    for (int i = 0; i < slice1.height; i++)
-    {
-        for (int j = 0; j < slice1.width; j++)
-        {
-            printf("%f ", errors[i * slice1.width + j]);
-        }
-        printf("\n");
-    }
-    printf("\n");
-    free(errors);
+    stbi_write_png("in2.jpg", imslice2.width, imslice2.height, imslice2.channels, out_image, imslice2.channels * imslice2.width);
+
+    // // test l2norm
+    // printf("%f\n", l2norm(slice, slice));
+    // pixel_t arr1[] = {1.0, 1.0, 1.0, 1.0};
+    // pixel_t arr2[] = {0.0, 0.0, 0.0, 0.0};
+    // slice_t s1 = {arr1, 2, 2, 1, 2};
+    // slice_t s2 = {arr2, 2, 2, 1, 2};
+    // printf("%f\n\n", l2norm(s1, s2));
+
+    // // test dpcut
+    // pixel_t matrix1[72] = {0.0};
+    // pixel_t matrix2[72] = {1.0};
+    // pixel_t out[72] = {0.0};
+
+    // for (int i = 0; i < 72; i++){
+    //     matrix2[i] = 1;
+    // }
+    // for (int i = 0; i < 6; i++)
+    // {
+    //     for (int j = 0; j < 4; j++)
+    //     {
+    //         for (int k = 0; k < 3; k++)
+    //         {
+    //             if (j == 1)
+    //             {
+    //                 matrix1[i * 4 * 3 + j*3 + k] = 0.5;
+    //             }
+    //             else if (j == 2)
+    //             {
+    //                 matrix1[i * 4 * 3 + j*3 + k] = 0.8;
+    //             }
+    //         }
+    //     }
+    // }
+    // slice_t slice1 = {matrix1, 4, 6, 3, 12};
+    // slice_t slice2 = {matrix2, 4, 6, 3, 12};
+    // slice_t slice_out = {out, 4, 6, 3, 12};
+    // for (int i = 0; i < slice1.height; i++)
+    // {
+    //     for (int j = 0; j < slice1.width*3; j += 3)
+    //     {
+    //         printf("[%f, %f, %f] ", slice1.data[i * slice1.jumpsize + j], slice1.data[i * slice1.jumpsize + j + 1], slice1.data[i * slice1.jumpsize + j + 2]);
+    //     }
+    //     printf("\n");
+    // }
+    // printf("\n");
+    // for (int i = 0; i < slice2.height; i++)
+    // {
+    //     for (int j = 0; j < slice2.width * 3; j += 3)
+    //     {
+    //         printf("[%f, %f, %f] ", slice2.data[i * slice2.jumpsize + j], slice2.data[i * slice2.jumpsize + j + 1], slice2.data[i * slice2.jumpsize + j + 2]);
+    //     }
+    //     printf("\n");
+    // }
+    // printf("\n");
     
+    // // for printing out array
+    // dpcut(slice1, slice2, slice_out, 0);
+    // for (int i = 0; i < slice_out.height; i++)
+    // {
+    //     for (int j = 0; j < slice_out.width * 3; j += 3)
+    //     {
+    //         printf("[%f, %f, %f] ", slice_out.data[i * slice_out.jumpsize + j], slice_out.data[i * slice_out.jumpsize + j + 1], slice_out.data[i * slice_out.jumpsize + j + 2]);
+    //     }
+    //     printf("\n");
+    // }
+    // printf("\n");
+    
+    // exit(-1);
+
+    // // for printing dp array
+    // pixel_t *dp = dpcut(slice1, slice2, slice_out, 0);
+    // for (int i = 0; i < slice1.height; i++)
+    // {
+    //     for (int j = 0; j < slice1.width; j++)
+    //     {
+    //         printf("%f ", dp[i * slice1.width + j]);
+    //     }
+    //     printf("\n");
+    // }
+    // printf("\n");
+    
+    // exit(-1);
+
+    
+    // // for printing errors array
+    // pixel_t *errors = dpcut(slice1, slice2, slice_out, 0);
+    // for (int i = 0; i < slice1.height; i++)
+    // {
+    //     for (int j = 0; j < slice1.width; j++)
+    //     {
+    //         printf("%f ", errors[i * slice1.width + j]);
+    //     }
+    //     printf("\n");
+    // }
+    // printf("\n");
+    
+    // exit(-1);
+    //free(errors);
+
     // print input image
     imwrite(in_image, "out.png");
     imfree(in_image);
@@ -157,4 +242,5 @@ int main()
     return 0;
 }
 
-// gcc test.c L2norm.c dpctu.c -o test -lm
+// gcc imageio.c L2norm.c dpcut.c -o test -lm
+
