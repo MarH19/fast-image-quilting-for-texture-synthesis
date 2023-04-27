ac37d11028b1cbdaaad0405ff3433d98b4d20a3e
Christof <christof.leutenegger@hotmail.com>
2023-04-27 20:07:27 +0200

    Moved code to data,include,src,tests folders

---
+++ src/L2norm.c
@@ -0,0 +1,23 @@
+#include "image_quilting.h"
+#include <math.h>
+#include <assert.h>
+
+double l2norm(slice_t s1, slice_t s2)
+{
+    double error = 0;
+    assert(s1.width == s2.width);
+    assert(s1.height == s2.height);
+    assert(s1.channels == s2.channels);
+
+    for (int i = 0; i < s1.height; i++)
+    {
+        for (int j = 0; j < s1.channels * s1.width; j++)
+        {
+            double s1_data = s1.data[i * s1.jumpsize + j];
+            double s2_data = s2.data[i * s2.jumpsize + j];
+
+            error += pow(s1_data - s2_data, 2);
+        }
+    }
+    return sqrt(error);
+}
---
+++ src/test.c
@@ -0,0 +1,15 @@
+#include "image_quilting.h"
+
+int main()
+{
+    int blocksize = 35;
+    int num_blocks = 10;
+    int overlap = blocksize / 6;
+    pixel_t tolerance = 0.3;
+
+    image_t in = imread("data/floor.jpg");
+    image_t out = image_quilting(in, blocksize, num_blocks, overlap, tolerance);
+    imwrite(out, "data/out.jpg");
+}
+// gcc -I./include src/test.c src/image_quilting.c src/dpcut.c src/imageio.c src/L2norm.c -lm
+// gcc src/test.c src/image_quilting.c src/dpcut.c src/imageio.c src/L2norm.c  -o test -lm
