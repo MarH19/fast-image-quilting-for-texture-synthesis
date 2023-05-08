a8fecbf3fbdb3277b23ffe1c5d6ee2bc49ac5052
Christof <christof.leutenegger@hotmail.com>
2023-05-08 15:40:44 +0200

    fix: remove leak in tests/test.c
    Added the remaining imageio.c functions to header to make them accessible.

--- include/image_quilting.h
+++ include/image_quilting.h
@@ -26,13 +26,23 @@ typedef struct
     int col;
 } coord;
 
-image_t imread(char *path);
-void imwrite(image_t image, char *path);
-pixel_t l2norm(slice_t, slice_t);
-void dpcut(slice_t slice_1, slice_t slice_2, slice_t out, int left2right);
+/* imageio.c */
+image_t imread(char *path);              /* read image at path */
+void imwrite(image_t image, char *path); /* write image to path*/
+void imfree(image_t image);              /* free memory of the image */
+void slice_cpy(slice_t in, slice_t out); /* copy slice from in into out */
 slice_t slice_image(image_t image, int start_row, int start_col, int end_row, int end_col);
+slice_t slice_slice(slice_t sin, int start_row, int start_col, int end_row, int end_col);
+
+/* l2norm.c */
+pixel_t l2norm(slice_t, slice_t);        /* calculate L2 norm between the two slices */
+
+/* dpcut.c */
+/* dpcut: calulate minimal cut between slice_1 and slice_2 and put it into out */
+void dpcut(slice_t slice_1, slice_t slice_2, slice_t out, int left2right);
+
+/* image_quilting.c */
 image_t image_quilting(image_t in, int blocksize, int num_blocks, int overlap, pixel_t tolerance);
-void slice_cpy(slice_t in, slice_t out);
 
 #define REL_TOL 1e-9
 #define ABS_TOL 0.0
--- tests/test.c
+++ tests/test.c
@@ -10,6 +10,8 @@ int main()
     image_t in = imread("data/floor.jpg");
     image_t out = image_quilting(in, blocksize, num_blocks, overlap, tolerance);
     imwrite(out, "data/out.jpg");
+    imfree(in);
+    imfree(out);
 }
 // gcc -I./include src/test.c src/image_quilting.c src/dpcut.c src/imageio.c src/L2norm.c -lm
 // gcc src/test.c src/image_quilting.c src/dpcut.c src/imageio.c src/L2norm.c  -o test -lm
